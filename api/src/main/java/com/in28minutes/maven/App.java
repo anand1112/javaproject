5e9e9555a2ecd92ad64f07d828961046da77f775
Your Name <you@example.com>
2020-03-10 08:51:36 +0000

    changed local again

--- api/src/main/java/com/in28minutes/maven/App.java
+++ api/src/main/java/com/in28minutes/maven/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
          System.out.println( "This is sample Java application" );
-        System.out.println( "Hello World Test My Code");
+        System.out.println( "Hello World Test My Code..... ");
     }
 
 	public int calculateSomething() {
